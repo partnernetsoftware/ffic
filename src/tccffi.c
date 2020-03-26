2be3bb857a9bf7b5c4c7aad2276b59feca91da8d
wanjochan <wanjochan@gmail.com>
2020-03-26 18:35:50 +0800

    hide the debug entries

--- src/tccffi.c
+++ src/tccffi.c
@@ -23,13 +23,13 @@ int main(int argc, char **argv){
 	tcc(tcc_add_symbol)(s, "ffi_raw", ffi_raw);
 
 	//{
-	tcc(tcc_add_symbol)(s, "ffi_printf", printf);
-	tcc(tcc_add_symbol)(s, "ffi_libc_printf", libc(printf));
+	//tcc(tcc_add_symbol)(s, "ffi_printf", printf);
+	//tcc(tcc_add_symbol)(s, "ffi_libc_printf", libc(printf));
 
-	tcc(tcc_add_symbol)(s, "ffi_fprintf", fprintf);
-	tcc(tcc_add_symbol)(s, "ffi_libc_fprintf", libc(fprintf));
-	//tcc(tcc_add_symbol)(s, "ffi_libc_fprintf", ffi("msvcrt","fprintf"));
-	tcc(tcc_add_symbol)(s, "ffi_std", ffi_std);
+	//tcc(tcc_add_symbol)(s, "ffi_fprintf", fprintf);
+	//tcc(tcc_add_symbol)(s, "ffi_libc_fprintf", libc(fprintf));
+	////tcc(tcc_add_symbol)(s, "ffi_libc_fprintf", ffi("msvcrt","fprintf"));
+	//tcc(tcc_add_symbol)(s, "ffi_std", ffi_std);
 	//}
 
 	if (tcc(tcc_relocate)(s, 1/*TCC_RELOCATE_AUTO*/) < 0) return 2;
