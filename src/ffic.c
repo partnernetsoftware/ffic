e282e8a426e18f910dd946132740d163a59f2bad
wanjochan <wanjochan@gmail.com>
2020-03-29 17:32:52 +0800

    improve for win

--- src/ffic.c
+++ src/ffic.c
@@ -8,21 +8,22 @@ int main(int argc, char **argv){
 	anyptr tcc_ptr = tcc(tcc_new)();
 	if (!tcc_ptr) { return 1; }
 
-	tcc(tcc_define_symbol)(tcc_ptr, "FFIC", "2");
 	tcc(tcc_set_output_type)(tcc_ptr, 1/*TCC_OUTPUT_MEMORY*/);
 
-	tcc(tcc_set_options)(tcc_ptr, "-nostdlib");
 	tcc(tcc_set_options)(tcc_ptr, "-nostdinc");
-
-#define _WIN32
+#ifdef _WIN32
+	//tcc(tcc_set_options)(tcc_ptr, "-nostdlib");
 	tcc(tcc_set_options)(tcc_ptr, "-D_WIN32");
 	tcc(tcc_set_options)(tcc_ptr, "-DTCC_TARGET_PE");
+#else
+	//tcc(tcc_set_options)(tcc_ptr, "-nostdlib");
 #endif
-#define _WIN64
+#ifdef _WIN64
 	tcc(tcc_set_options)(tcc_ptr, "-D_WIN64");
 #endif
-#define _APPLE_
+#ifdef _APPLE_
 	tcc(tcc_set_options)(tcc_ptr, "-D_APPLE_");
+	tcc(tcc_set_options)(tcc_ptr, "-DTCC_TARGET_MACHO");
 #endif
 	//tcc(tcc_set_options)(tcc_ptr, "-run");
 
@@ -30,6 +31,8 @@ int main(int argc, char **argv){
 	tcc(tcc_add_symbol)(tcc_ptr, "ffic_raw", ffic_raw);
 
 	tcc(tcc_add_file)(tcc_ptr,(argc>1) ? argv[1] : "-");
+
+	tcc(tcc_define_symbol)(tcc_ptr, "FFIC", "2");
 
 	if (tcc(tcc_relocate)(tcc_ptr, 1/*TCC_RELOCATE_AUTO*/) < 0) return 2;
 	anyptr (*entry)() = tcc(tcc_get_symbol)(tcc_ptr, "main");
