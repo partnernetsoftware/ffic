8d1a6f461eac8a54bc137182ea32e8299c0c7f14
wanjochan <wanjochan@gmail.com>
2020-04-05 14:43:15 +0800

    win32

--- src/test_sao.bat
+++ src/test_sao.bat
@@ -1,4 +1,4 @@
 @rem build sao && upx sao_win32.exe && dir sao*.exe && type fib.scm | sao_win32
 @rem build sao && upx sao_win32.exe && dir sao*.exe && type fib.sao | sao_win32
 @rem build sao && upx sao_win32.exe && dir sao*.exe && type test.sao | sao_win32 -i -p -v
-build sao && upx sao_win32.exe && dir sao*.exe && type test.sao | sao_win32 i p v
+build sao && upx sao_win32.exe && dir sao*.exe && type tests\test.sao | sao_win32 i p v
