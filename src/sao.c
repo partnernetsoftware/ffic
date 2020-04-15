c04bff50cbd02f2d112d125d1eb63d72b0cbb188
wanjochan <wanjochan@gmail.com>
2020-04-15 22:11:42 +0800

    WTF:

--- src/sao.c
+++ src/sao.c
@@ -423,8 +423,16 @@ p_sao_obj sao_load_expr(sao_stream * fw) //TODO add ,depth ?
 			
 			if(SAO_ARGV(l)){ return theSymbol; }//LISP
 			else{
-				if(libc(strchr)(" \t\r\n(", sao_peek(fw)))continue;
-				return theSymbol;
+				while( (libc(strchr)(" \t", sao_peek(fw))) ) c = sao_deq_c(fw);
+				//if (c!='(') return theSymbol;
+				//while(' '==sao_peek(fw)) c = sao_deq_c(fw);//TODO support \t later
+				if('('==sao_peek(fw)){
+					c = sao_deq_c(fw);//jump next
+				}else{
+					return theSymbol;
+				}
+//				if(libc(strchr)(" \t\r\n(", sao_peek(fw)))continue;
+//				return theSymbol;
 			}
 		}
 		if (c == '(') {
@@ -437,7 +445,8 @@ p_sao_obj sao_load_expr(sao_stream * fw) //TODO add ,depth ?
 		if (c == ')') {
 			return SAO_NULL; //TODO check depth, and should return error if the pracket is not correct
 		}
-		return SAO_NULL;
+		if(theSymbol) return theSymbol;
+		//return SAO_NULL;
 	}
 	return SAO_NULL;
 }
@@ -534,38 +543,36 @@ int main(int argc,char **argv, char** envp) {
 	if(argc>1){
 		char argv_line[512] = "_(";//{'_','(',0};
 		ffic_string argv_ptr = &argv_line[2];
-		for(int i=1;i<argc;i++){*argv_ptr++=' ';ffic_string wk=argv[i];while(*wk)*argv_ptr++=*wk++;}
+		for(int i=1;i<argc;i++){ (*argv_ptr++)=' ';ffic_string wk=argv[i];while(*wk) (*argv_ptr++)=(*wk++);}
 		*argv_ptr++ = ')'; *argv_ptr++ = '\0';
+		sao_stdout("DEBUG 001 argv_line=%s\n",argv_line);
 		sao_stream * fw = sao_stream_new(argv_line,stream_char);
 		p_sao_obj arg_expr = sao_load_expr( fw );
+		sao_out_expr("\nDEBUG arg_expr=",arg_expr);
 		p_sao_obj pos = cdr(arg_expr);
 		while(pos){
-			//sao_out_expr("DEBUG pos=",pos);
+			sao_out_expr("\nDEBUG pos=",pos);
 			p_sao_obj _car = car(pos);
 			//sao_out_expr("\nDEBUG _car=",_car);
-			ffic_string string_or_name="-";
-			int l_val = 0;
+			ffic_string string_or_name;
+			int l_val = 1;
 			if(sao_is_list(_car)){
 				p_sao_obj _caar = car(_car);
-				//string_or_name = _caar->_string;
 				string_or_name = _caar->_raw;
 				p_sao_obj _cadar = car(cdr(_car));
 				//l_val = (_cadar && _cadar->_type==type_integer) ? _cadar->_integer : 0;
 				if(_cadar) l_val = (long) libc(atol)(_cadar->_raw);
 			}else{
-				if(_car){
-					string_or_name = _car->_string;
-					l_val = 1;
-				}
+				string_or_name = _car->_string;
 			}
-			//sao_stdout("DEBUG 002 string_or_name=%s,l_val=%ld\n",string_or_name,l_val);
-			if(string_or_name){
+			sao_stdout("DEBUG 002 string_or_name=%s,l_val=%ld\n",string_or_name,l_val);
+			//if(string_or_name){
 				sao_var(sao_new_symbol(string_or_name), sao_new_integer(l_val), SAO_TAG_argv);
 				int found = 0;
 				for(int i=0;i<=argt_h;i++) if(!strcmp(string_or_name,argt_names[i])){ argta[i]+=l_val; found=1;break; }
 				if(!found) script_file = string_or_name; else found_any++;
-			}
-			//sao_stdout("DEBUG 003 string_or_name=%s,l_val=%ld\n",string_or_name,l_val);
+			//}
+			sao_stdout("DEBUG 003 string_or_name=%s,l_val=%ld\n",string_or_name,l_val);
 			pos = cdr(pos);
 		}
 		libc(free)(fw);//
