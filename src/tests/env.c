82f228ebb274dcaf0c4f89b36adf1b2e957ab82c
wanjochan <wanjochan@gmail.com>
2020-09-14 22:49:10 +0800

    WTF:

--- src/tests/env.c
+++ src/tests/env.c
@@ -1,3 +1,7 @@
+#define dump_ld(v) printf(#v "=%ld\n",(long int)v)
+#define dump_d(v) printf(#v "=%d\n",(int)v)
+#define dump_s(v) printf(#v "=%s\n",(char*)v)
+#define dump(v,t) dump##_##t(v)
 int main(int argc, char* argv[], char** envp){
 	extern int printf(const char*,...);
 
@@ -67,7 +71,24 @@ int main(int argc, char* argv[], char** envp){
 	//char buffer[255];
 	//getcwd(buffer,255);
 	//printf("getcwd=%s\n",buffer);
-
+typedef void* ffic_ptr;
+typedef ffic_ptr(
+#ifdef _WIN32
+		__attribute__((__stdcall__))
+#endif
+		*ffic_func)();
+	extern ffic_func(*ffic(const char*, const char*))();
+	char path[512]; int size = 512;
+#ifdef _WIN32
+	ffic_func c_GetModuleFileName = ffic("kernel32","GetModuleFileNameA");
+	dump(c_GetModuleFileName,ld);
+	c_GetModuleFileName(0,path,&size);
+#else
+	ffic_func c___NSGetExecutablePath = ffic("libc","_NSGetExecutablePath");
+	dump(c___NSGetExecutablePath,ld);
+	c___NSGetExecutablePath(path,&size);
+#endif
+	dump(path,s);
 	//TODO
 	//Mac OS X: _NSGetExecutablePath() (man 3 dyld)
 	//Linux: readlink /proc/self/exe
