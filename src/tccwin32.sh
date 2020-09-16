74a7f26d9b55a10cdd80a4d45d896014d19916f9
wanjochan <wanjochan@gmail.com>
2020-09-17 07:59:51 +0800

    .exe.i

--- src/tccwin32.sh
+++ src/tccwin32.sh
@@ -1,6 +1,6 @@
 R_=$(pwd)
 
-echo `dirname $0`
+#echo `dirname $0`
 #pwd
 
 #RTCC=$(cd `dirname $0`/../tinycc; pwd)
@@ -10,7 +10,7 @@ RTCC=../tinycc
 CC="$RTCC/../tcc_osx/i386-win32-tcc -L. -DCONFIG_LDDIR="\".\"" -DTCC_LIBTCC1="\"i386-win32-libtcc1.a\"" -I $RTCC/win32/include -I $RTCC/win32/include/winapi -I $RTCC/include -I $RTCC -I $RTCC -B $RTCC/../bin -L $RTCC/../bin -L $RTCC/win32/lib -L. -I."
 #CC="$RTCC/../tcc_osx/i386-win32-tcc -L. -DCONFIG_LDDIR="\".\"" -I $RTCC/win32/include -I $RTCC/win32/include/winapi -I $RTCC/include -I $RTCC -I $RTCC -B $RTCC/../bin -L $RTCC/../bin -L $RTCC/win32/lib -L. -I."
 
-echo $CC $*
+#echo $CC $*
 $CC $*
 
 cd $R_
