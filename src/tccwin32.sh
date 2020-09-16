0847f3292365efd6a33a3c23205cc62aebcbefda
wanjochan <wanjochan@gmail.com>
2020-09-17 07:52:27 +0800

    failed: direct build failed

--- src/tccwin32.sh
+++ src/tccwin32.sh
@@ -1,10 +1,16 @@
 R_=$(pwd)
 
-RTCC=$(cd `dirname $0`/../tinycc; pwd)
+echo `dirname $0`
+#pwd
+
+#RTCC=$(cd `dirname $0`/../tinycc; pwd)
+RTCC=../tinycc
 #echo RTCC=$RTCC
 
 CC="$RTCC/../tcc_osx/i386-win32-tcc -L. -DCONFIG_LDDIR="\".\"" -DTCC_LIBTCC1="\"i386-win32-libtcc1.a\"" -I $RTCC/win32/include -I $RTCC/win32/include/winapi -I $RTCC/include -I $RTCC -I $RTCC -B $RTCC/../bin -L $RTCC/../bin -L $RTCC/win32/lib -L. -I."
+#CC="$RTCC/../tcc_osx/i386-win32-tcc -L. -DCONFIG_LDDIR="\".\"" -I $RTCC/win32/include -I $RTCC/win32/include/winapi -I $RTCC/include -I $RTCC -I $RTCC -B $RTCC/../bin -L $RTCC/../bin -L $RTCC/win32/lib -L. -I."
 
+echo $CC $*
 $CC $*
 
 cd $R_
--- src/tccwin64.sh
+++ src/tccwin64.sh
@@ -1,10 +1,15 @@
 R_=$(pwd)
 
-RTCC=$(cd `dirname $0`/../tinycc; pwd)
+cd $0
+pwd
+
+#RTCC=$(cd `dirname $0`/../tinycc; pwd)
+RTCC=../tinycc
 #echo RTCC=$RTCC
 
 CC="$RTCC/../tcc_osx/x86_64-win32-tcc -L. -DCONFIG_LDDIR="\".\"" -DTCC_LIBTCC1="\"x86_64-win32-libtcc1.a\"" -I $RTCC/win32/include -I $RTCC/win32/include/winapi -I $RTCC/include -I $RTCC -I $RTCC -B $RTCC/../bin -L $RTCC/../bin -L $RTCC/win32/lib -L. -I."
 
+echo $CC $*
 $CC $*
 
 cd $R_
