6824038893dbcd42db801bed49d49796cefdcf71
partnernetsoftware <business@partnernetsoftware.com>
2020-09-25 00:39:49 +0800

    cuda.c (notes only)

---
+++ src/lab/cuda.c
@@ -0,0 +1,7 @@
+# lrwxrwxrwx 1 root root       12 Aug 20  2019 libcuda.so -> libcuda.so.1
+# lrwxrwxrwx 1 root root       18 Aug 20  2019 libcuda.so.1 -> libcuda.so.410.129
+# -rw-r--r-- 1 root root 15673048 Aug 20  2019 libcuda.so.410.129
+# -rw-r--r-- 1 root root   292840 Aug 20  2019 libnvidia-fatbinaryloader.so.410.129
+# lrwxrwxrwx 1 root root       29 Aug 20  2019 libnvidia-ptxjitcompiler.so -> libnvidia-ptxjitcompiler.so.1
+# lrwxrwxrwx 1 root root       35 Aug 20  2019 libnvidia-ptxjitcompiler.so.1 -> libnvidia-ptxjitcompiler.so.410.129
+# -rw-r--r-- 1 root root 12129448 Aug 20  2019 libnvidia-ptxjitcompiler.so.410.129
